d5560d4ca75c707036830d6ef2344e1ce19abdc1
ritesh singh <riteshf7@gmail.com>
2025-11-08 16:51:04 +0530

    added kotlin code

---
+++ app/src/main/java/com/termux/app/SampleKotlin.kt
@@ -0,0 +1,9 @@
+package com.termux.app
+
+/**
+ * Simple Kotlin class used to verify that Kotlin sources compile correctly.
+ */
+class SampleKotlin {
+    fun greet(name: String = "Termux"): String = "Hello, $name from Kotlin!"
+}
+
